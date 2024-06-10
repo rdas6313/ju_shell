6e112e31ae2a800fca294135ebc28d2c878047a1
Raja Das <rdas6313@gmail.com>
2024-06-10 21:23:44 +0530

    refactor: question 3 of assignment 4

--- assignment4/q3.c
+++ assignment4/q3.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 #define MAX 100
 int parToch[2];
 int chTopar[2];
 
-int is_palidrome(char data[]){
+int is_palidrome(char data[])
+{
 	int len = strlen(data);
 	int i = 0, j = len - 1;
-	while(i < j){
-		if(data[i] != data[j])
+	while (i < j)
+	{
+		if (data[i] != data[j])
 			return 0;
 		i++;
 		j--;
@@ -18,70 +20,81 @@ int is_palidrome(char data[]){
 	return 1;
 }
 
-int main(){
+int main()
+{
 
-	if(pipe(parToch) < 0 || pipe(chTopar) < 0){
+	if (pipe(parToch) < 0 || pipe(chTopar) < 0)
+	{
 		printf("Error: Unable to create pipe\n");
 		return 0;
 	}
 
 	int pid = fork();
-	if(pid < 0){
+	if (pid < 0)
+	{
 		printf("Error:Unable to create child\n");
 		return 0;
-	}else if(pid > 0){
-		//parent
+	}
+	else if (pid > 0)
+	{
+		// parent
 		char data[MAX];
 		printf("Enter a string: ");
-		scanf("%s",data);
+		scanf("%s", data);
 
-		//sending data to child
+		// sending data to child
 		close(parToch[0]);
-		int bytes = write(parToch[1],data,strlen(data));
-		if(bytes < 0){
+		int bytes = write(parToch[1], data, strlen(data));
+		if (bytes < 0)
+		{
 			printf("Error: unable to send data to child\n");
 			return 0;
 		}
-		printf("Sent %d bytes data to child\n",bytes);
-		//receiving data from child
-		memset(data,'\0',sizeof(data));
+		printf("Sent %d bytes data to child\n", bytes);
+		// receiving data from child
+		memset(data, '\0', sizeof(data));
 		close(chTopar[1]);
-		bytes = read(chTopar[0],data,MAX);
-		if(bytes < 0){
+		bytes = read(chTopar[0], data, MAX);
+		if (bytes < 0)
+		{
 			printf("Error: Unable to receive data from child\n");
 			return 0;
 		}
-		printf("Received %d data from child\n",bytes);
-		printf("Palindrome: %s\n",data);
-
-	}else{
+		printf("Received %d bytes data from child\n", bytes);
+		printf("Palindrome: %s\n", data);
+	}
+	else
+	{
 		char data[MAX];
-		//receiving data from parent
+		// receiving data from parent
 		close(parToch[1]);
-		int bytes = read(parToch[0],data,MAX);
-		if(bytes < 0){
+		int bytes = read(parToch[0], data, MAX);
+		if (bytes < 0)
+		{
 			printf("Error: Unable to receive data from parent\n");
 			return 0;
 		}
-		printf("Received %d data from parent\n",bytes);
-		//check for palindrome
+		printf("Received %d bytes data from parent\n", bytes);
+		// check for palindrome
 		char response[5];
-		memset(response,'\0',sizeof(response));
-		if(is_palidrome(data)){
-			strcpy(response,"YES");
-		}else{
-			strcpy(response,"NO");
+		memset(response, '\0', sizeof(response));
+		if (is_palidrome(data))
+		{
+			strcpy(response, "YES");
 		}
-				//send data to parent
+		else
+		{
+			strcpy(response, "NO");
+		}
+		// send data to parent
 		close(chTopar[0]);
-		bytes = write(chTopar[1],response,strlen(response));
-		if(bytes < 0){
+		bytes = write(chTopar[1], response, strlen(response));
+		if (bytes < 0)
+		{
 			printf("Error: unable to send data to parent\n");
 			return 0;
 		}
-		printf("Sent %d bytes data to parent\n",bytes);
-
-
+		printf("Sent %d bytes data to parent\n", bytes);
 	}
 	return 0;
 }
