3bf10b0260eaa5c8903ce2791a168cad7012a39d
Raja <rdas6313@gmail.com>
2024-06-15 16:43:18 +0530

    change implementation of question 3 of assignment 2

--- assignment2/a3.sh
+++ assignment2/a3.sh
@@ -1,10 +1,6 @@
-
-
-read -ra lines <<< $(cat /proc/cpuinfo)
-
-echo "${lines[3]} ${lines[4]} ${lines[5]}"
-echo "${lines[6]} ${lines[7]} ${lines[8]} ${lines[9]}"
-echo "${lines[13]} ${lines[14]} ${lines[15]} ${lines[16]} ${lines[17]} ${lines[18]} ${lines[19]} ${lines[20]} ${lines[21]}"
-echo "${lines[28]} ${lines[29]} ${lines[30]} ${lines[31]}"
-echo "${lines[32]} ${lines[33]} ${lines[34]} ${lines[35]} ${lines[36]} "
-  
+IFS=$'\n'
+lines=( $(cat /proc/cpuinfo) )
+echo "${lines[1]}"
+echo "${lines[4]}"
+echo "${lines[7]}"
+echo "${lines[8]}"
